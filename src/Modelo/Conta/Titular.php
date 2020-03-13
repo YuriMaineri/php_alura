7d916b0ef6ae49f35d689a67a9c63f38c7744001
Yuri Maineri <yurimaineri5@gmail.com>
2020-03-13 18:07:47 -0300

    Arquivo de autoload da aplicacao

---
+++ src/Modelo/Conta/Titular.php
@@ -0,0 +1,23 @@
+<?php
+
+
+class Titular extends Pessoa
+{
+    private $endereco;
+
+    public function __construct(Cpf $cpf, string $nome, Endereco $endereco)
+    {
+        parent::__construct($nome, $cpf);
+        $this->endereco = $endereco;
+    }
+
+    public function recuperaEndereco(): Endereco
+    {
+        return $this->endereco;
+    }
+
+
+
+
+
+}
