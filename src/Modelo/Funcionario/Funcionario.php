7863144a7335ba745e994fd0070595d544c27658
Yuri Maineri <yurimaineri5@gmail.com>
2020-03-15 17:51:58 -0300

    Criacao de classes separadas para cargos

---
+++ src/Modelo/Funcionario/Funcionario.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Alura\Banco\Modelo\Funcionario;
+
+
+use Alura\Banco\Modelo\Cpf;
+use Alura\Banco\Modelo\Pessoa;
+
+abstract class Funcionario extends Pessoa
+{
+    private $cargo;
+    private $salario;
+
+    public function __construct(string $nome, Cpf $cpf, string $cargo, float $salario)
+    {
+        parent::__construct($nome, $cpf);
+        $this->cargo = $cargo;
+        $this->salario = $salario;
+    }
+
+    public function recuperaCargo(): string
+    {
+        return $this->cargo;
+    }
+
+    public function alteraNome(string $nome): void
+    {
+        $this->validaNomeTitular($nome);
+        $this->nome = $nome;
+    }
+
+    public function recuperaSalario(): float
+    {
+        return $this->salario;
+    }
+
+    public function recebeAumento(float $valorAumento): void
+    {
+        if ($valorAumento < 0) {
+            echo "Salário precisa ser positivo";
+            return;
+        }
+
+        $this->salario += $valorAumento;
+    }
+
+    public function calculaBonificacao(): float
+    {
+        return $this->salario * 0.1;
+    }
+
+}
